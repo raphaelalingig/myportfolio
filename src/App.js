01b29ea03e094a2a54013553b281067bb7495b48
Raphael Alingig <124657190+raphaelalingig@users.noreply.github.com>
2024-12-26 00:43:04 +0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+import "./index.css";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
